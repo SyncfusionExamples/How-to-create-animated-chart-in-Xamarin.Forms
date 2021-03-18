1013f474017c97cfa6a83b64e53080a9eeaac356
Hemalatha Marikumar <hemalatha.marikumar@syncfusion.com>
2021-03-18 17:08:54 +0530

    Sample included

---
+++ ChartSample/ChartSample/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ChartSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            //Data is a collection of Model which is populated to the series.
+            (BindingContext as ViewModel).Data.Add(new ChartModel() { XValue = "Aug", YValue1 = 90 });
+            //Animate() is used to animate the series
+            series.Animate();
+        }
+    }
+}
---
+++ ChartSample/ChartSample/ViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Syncfusion.SfChart.XForms;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace ChartSample
+{
+    public class ChartModel
+    {
+        public string XValue { get; set; }
+
+        public double YValue1 { get; set; }
+    
+    }
+    public class ViewModel
+    {
+        public ObservableCollection<ChartModel> Data { get; set; }
+
+        public ViewModel()
+        {
+            Data = new ObservableCollection<ChartModel>();
+
+            Data.Add(new ChartModel() { XValue = "Jan", YValue1 = 37 });
+            Data.Add(new ChartModel() { XValue = "Feb", YValue1 = 37 });
+            Data.Add(new ChartModel() { XValue = "Mar", YValue1 = 39 });
+            Data.Add(new ChartModel() { XValue = "Apr", YValue1 = 43 });
+            Data.Add(new ChartModel() { XValue = "May", YValue1 = 48 });
+            Data.Add(new ChartModel() { XValue = "Jun", YValue1 = 54 });
+            Data.Add(new ChartModel() { XValue = "Jul", YValue1 = 57 });
+        }
+    }
+}
